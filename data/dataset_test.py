4c42842c6295b6b944d8d5a3378ac27385769063
naikaida <np@uni-kassel.de>
2024-06-18 20:37:41 +0200

    HF, sampling and listening test

--- data/dataset_test.py
+++ data/dataset_test.py
@@ -2,3 +2,16 @@
 from datasets import load_dataset
 
 dataset = load_dataset('DBD-research-group/BirdSet','HSN')
+
+
+#%%
+import soundfile as sf
+import matplotlib.pyplot as plt
+
+data, samplerate = sf.read(dataset['train'][1]['filepath'])
+#%%
+plt.plot(data)
+
+#%%
+from IPython.display import Audio
+Audio(data, rate=samplerate)
